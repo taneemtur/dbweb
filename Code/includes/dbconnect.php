5f99a3c19a37cc42d2655d39e67f4cbbe06bf4c7
taneemtur <36967746+taneemtur@users.noreply.github.com>
2019-11-18 22:02:54 +0500

    new

---
+++ Code/includes/dbconnect.php
@@ -0,0 +1,17 @@
+<?php
+
+    $server = 'localhost';
+    $user = '';
+    $password = '';
+    $db = 'bmu_bank';
+
+    
+    $con = mysqli_connect($server, $user, $password, $db);
+
+    if(!$con){
+        
+        die('Could not connect');
+    }
+
+
+?>
